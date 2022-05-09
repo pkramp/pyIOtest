9f402b0f88a18351c4ecb93378472e6a3fb94958
pkramp <p.n.kramp@gsi.de>
2022-05-09 16:44:19 +0200

    Restructure and modularize

--- pyIOtest.py
+++ pyIOtest.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 from datetime import datetime
+import shutil
 
 test_results = []
 # parsing method for compileBench raw text
@@ -13,6 +14,18 @@ def parseCompileBench(outString):
     print(outString[pos3+4:pos4-2])
     test_results.append(outString[pos1+4:pos2-2])
     test_results.append(outString[pos3+4:pos4-2])
+
+def initializePath(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def cleanUp(path):
+    shutil.rmtree(path)
+
+# get all the outputs in a string and decode it to a string
+def decodeOutput(output):
+    return (output.stderr.read() + output.stdout.read()).decode("utf-8")
+    
 # runs the IO commands in subprocesses, catches all stderr and stdout output and
 # writes it into resultPath, automatically appending run to filename
 def runIoCommand(runs, command, resultPath):
@@ -23,42 +36,47 @@ def runIoCommand(runs, command, resultPath):
             print(command2)
             # start subprocess and capture
             output = subprocess.Popen(command2, stderr=subprocess.PIPE,stdout=subprocess.PIPE, shell=True)
-            # get all the outputs in a string
-            outString = b""
-            outString += output.stderr.read()        
-            outString += output.stdout.read()
-            outString = outString.decode("utf-8") 
             # write output
             f = open(resultPath + str(x) + ".txt", "w")
-            f.write(outString)
+            f.write(decodeOutput(output))
             f.close()
+
+def compileBenchTests():
+    compileBenchRuns = 1
+    # compilebench in makej mode
+    runIoCommand(compileBenchRuns, 'cd compilebench-0.6 && ./compilebench -D' + IOdirectory + 'compilebench_working_dir_<run> -i 1 --makej ',  resultDir + "/compilebench_makej_")
+    # without makej
+    runIoCommand(compileBenchRuns, 'cd compilebench-0.6 && ./compilebench -D' + IOdirectory + 'compilebench_working_dir_<run> -i 1 -r 1 ',  resultDir + "/compilebench_")
+    cleanUp(IOdirectory)
+    initializePath(IOdirectory)
+
+def ioZoneTests():
+    iozoneRuns = 1
+    # iozone with all tests
+    runIoCommand(iozoneRuns, 'iozone -a -b ' + resultDir + "/result<run>.xls",  resultDir + "/iozone_a")    
+
+def fioTests():
+    fioRuns = 1
+    # set fio parameters
+    fiomode = "randrw"
+    fionumj = "4"
+    fioSize = "20M"
+    fioBlockSize = "1M"
+    fioExtraArgs =  " --direct=0 --group_reporting --fallocate=none"
+    # run fio
+    runIoCommand(fioRuns, 'fio --rw=' + fiomode + " --name " + IOdirectory + "/test<run> --bs=" + fioBlockSize + " --numj=" + fionumj + " --size=" + fioSize + fioExtraArgs,
+      resultDir + "/fio_" + fiomode + "_s" + fioSize + "_numj" + fionumj + "_") 
+    cleanUp(IOdirectory)
 
 #"main"
 # create the test result directory with a timestamp
-testBaseDir = "/tmp/pyIOResults"
-if not os.path.exists(testBaseDir):
-    os.mkdir(testBaseDir)
+testBaseDir = "/tmp/pyIOResults/"
+initializePath(testBaseDir)
 resultDir = testBaseDir+ str(datetime.now())
-os.mkdir(resultDir)
-compileBenchRuns = 1
-iozoneRuns = 1
-IOdirectory = "/tmp/pyIOworkDir"
-if not os.path.exists(IOdirectory):
-    os.mkdir(IOdirectory)
-# compilebench in makej mode
-runIoCommand(compileBenchRuns, 'cd compilebench-0.6 && ./compilebench -D' + IOdirectory + 'compilebench_working_dir_<run> -i 1 --makej ',  resultDir + "/compilebench_makej_")
-# without makej
-runIoCommand(compileBenchRuns, 'cd compilebench-0.6 && ./compilebench -D' + IOdirectory + 'compilebench_working_dir_<run> -i 1 -r 1 ',  resultDir + "/compilebench_")
-# iozone with all tests
-runIoCommand(iozoneRuns, 'iozone -a -b ' + resultDir + ,  resultDir + "/iozone_a")
-# set fio parameters
-fiomode = "randrw"
-fionumj = "4"
-fioSize = "20M"
-fioBlockSize = "1M"
-fioExtraArgs =  " --direct=0 --group_reporting --fallocate=none"
-# run fio
-runIoCommand(2, 'fio --rw=' + fiomode + " --name test<run> --bs=" + fioBlockSize + " --numj=" + fionumj + " --size=" + fioSize + fioExtraArgs,
-  resultDir + "/fio_" + fiomode + "_s" + fioSize + "_numj" + fionumj + "_")
- 
-#subprocess.Popen("rm -rf test*", stderr=subprocess.PIPE,stdout=subprocess.PIPE, shell=True)
+initializePath(resultDir)
+IOdirectory = "/tmp/pyIOworkDir/"
+initializePath(IOdirectory)
+
+compileBenchTests()
+#ioZoneTests()
+fioTests()
